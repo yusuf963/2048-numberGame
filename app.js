4b94b68700d649c4ea2b10411cebcbef4ab24e3d
yusuf963 <mohammad963yusuf@gmail.com>
2021-01-15 12:04:49 +0000

    fixing when tile  function generate same position randomely

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const tiles = []
 for (let i = 0; i < width * width; i++ ) {
   const tile = document.createElement('div')  
   grid.appendChild(tile)
+  tile.innerHTML = 0
   tile.id = i + 1
   tile.classList.add('tile-styled')
   tiles.push(tile)
@@ -23,23 +24,32 @@ for (let i = 0; i < width * width; i++ ) {
 //starting the game by clicking on the start game button
 button.addEventListener('click', () =>{
 //mapping over the grid tiles New array 
-  let randomTile = tiles[Math.floor(Math.random() * tiles.length)]
-  console.log(randomTile)
+  const randomTile = tiles[Math.floor(Math.random() * tiles.length)]
+
   // rendering 2 or 4 value as number in 2 random tiles
-  function appendingRandomVAlueInRandomTile(){
+  function appendingRandomVAlueInRandomTile(value){
     const arrayValue = [2, 4]
     const randomValue = arrayValue[Math.floor(Math.random() * arrayValue.length )]
-    randomTile.append(randomValue)
+    value.append(randomValue)
+    //replacing the zero value with 2 or 4
+    value.innerHTML = randomValue
   }
   // rendering 2 or 4 value as number in 2 random tiles
-  appendingRandomVAlueInRandomTile()
-  console.log(randomTile)
+  appendingRandomVAlueInRandomTile(randomTile)
+  // console.log(randomTile)
   //mapping over the grid tiles New array 
-  randomTile = tiles[Math.floor(Math.random() * tiles.length)]
-  appendingRandomVAlueInRandomTile()
-  console.log(randomTile)
+
+  let randomTileSd = tiles[Math.floor(Math.random() * tiles.length)]
+  if ( randomTile === randomTileSd){
+    randomTileSd = tiles[Math.floor(Math.random() * tiles.length)]
+  }
+  appendingRandomVAlueInRandomTile(randomTileSd)
 })
 //open board finish
+
+
+
+// sliding rights, left and up and down  starts
 
 
 
