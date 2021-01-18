6f8a3699a9653d616c16675232311e04fbce3c23
yusuf963 <mohammad963yusuf@gmail.com>
2021-01-18 15:47:42 +0000

    caculationg the column tiles

--- testworkshop.js
+++ testworkshop.js
@@ -1,12 +1,21 @@
 const grid = document.getElementById('grid-container')
 const button = document.getElementById('new-game')
 const score = document.getElementById('score')
-
+const width = 4
 const gridTiles = []
 let tile
+// addin 2 or 4 to random tile position
+function adding2ToTheOpeningBoard() {
+  const randomTile = gridTiles[Math.floor(Math.random() * gridTiles.length)]
+  if (randomTile.innerHTML === '0') {
+    randomTile.innerHTML = Math.random() < 0.5 ? 2 : 4
+  } else {
+    adding2ToTheOpeningBoard()
+  }
+}
 
 //building the grid at the opening board game
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < width * width; i++) {
   tile = document.createElement('div')
   tile.innerHTML = 0
   tile.classList.add('tile-styled')
@@ -18,19 +27,12 @@ button.addEventListener('click', () => {
   gridTiles.forEach((tile) => {
     tile.innerHTML = 0
   })
-  function adding2ToTheOpeningBoard() {
-    const randomTile = gridTiles[Math.floor(Math.random() * gridTiles.length)]
-    if (randomTile.innerHTML === '0') {
-      randomTile.innerHTML = Math.random() < 0.5 ? 2 : 4
-    } else {
-      adding2ToTheOpeningBoard()
-    }
-  }
+
   adding2ToTheOpeningBoard()
   adding2ToTheOpeningBoard()
 
   //sliding the tils
-  function building4Arrays() {
+  function building4ArraysToRight() {
     for (let i = 0; i < gridTiles.length; i++) {
       if (i % 4 === 0) {
         const firstCol = gridTiles[i].innerHTML
@@ -38,7 +40,6 @@ button.addEventListener('click', () => {
         const thirdCol = gridTiles[i + 2].innerHTML
         const fourthCol = gridTiles[i + 3].innerHTML
         const rows = [Number(firstCol), Number(secondCol), Number(thirdCol), Number(fourthCol)]
-        console.log(rows)
         const sortedRow = rows.filter(number => number > 0)
         const emptyTile = 4 - sortedRow.length
         const filling = Array(emptyTile).fill(0)
@@ -47,14 +48,10 @@ button.addEventListener('click', () => {
         gridTiles[i + 1].innerHTML = newRow[1]
         gridTiles[i + 2].innerHTML = newRow[2]
         gridTiles[i + 3].innerHTML = newRow[3]
-        console.log(emptyTile)
-        console.log(sortedRow)
-        console.log(filling)
-        console.log(newRow)
       }
     }
-  } 
-  //  building4Arrays()
+  }
+  building4ArraysToRight()
 
   //sliding to left
   function building4ArraysToLeft() {
@@ -65,7 +62,6 @@ button.addEventListener('click', () => {
         const thirdCol = gridTiles[i + 2].innerHTML
         const fourthCol = gridTiles[i + 3].innerHTML
         const rows = [Number(firstCol), Number(secondCol), Number(thirdCol), Number(fourthCol)]
-        console.log(rows)
         const sortedRow = rows.filter(number => number > 0)
         const emptyTile = 4 - sortedRow.length
         const filling = Array(emptyTile).fill(0)
@@ -75,23 +71,96 @@ button.addEventListener('click', () => {
         gridTiles[i + 2].innerHTML = newRow[2]
         gridTiles[i + 3].innerHTML = newRow[3]
         console.log(emptyTile)
-        console.log(sortedRow)
-        console.log(filling)
-        console.log(newRow)
+
       }
     }
   } building4ArraysToLeft()
 
-  // adding the sum to the closest tile to the edge (with the sliding flow)
-  function sumToTiles(){
-    for (let i = 0; i < 15; i++){
-      if (gridTiles[i].innerHTML === gridTiles[i + 1].innerHTML){
-        const total = Number(gridTiles[i].innerHTML) + Number(gridTiles[i + 1].innerHTML)
-        gridTiles[i].innerHTML = total
-        gridTiles[i + 1].innerHTML = 0
 
-      }
+  // sliding down
+  function slidingDown() {
+    for (let i = 0; i < 4; i++) {
+      const firstCol = gridTiles[i].innerHTML
+      const secondCol = gridTiles[i + width].innerHTML
+      const thirdCol = gridTiles[i + width * 2].innerHTML
+      const fourthCol = gridTiles[i + width * 3].innerHTML
+      const col = [Number(firstCol), Number(secondCol), Number(thirdCol), Number(fourthCol)]
+
+      const sortedCol = col.filter(number => number > 0)
+      const emptyTile = 4 - sortedCol.length
+      const filling = Array(emptyTile).fill(0)
+      const newCol = filling.concat(sortedCol)
+      gridTiles[i].innerHTML = newCol[0]
+      gridTiles[i + width].innerHTML = newCol[1]
+      gridTiles[i + width * 2].innerHTML = newCol[2]
+      gridTiles[i + width * 3].innerHTML = newCol[3]
     }
   }
 
-// evert Listener
+  //sliding up 
+  function slidingDown() {
+    for (let i = 0; i < 4; i++) {
+      const firstCol = gridTiles[i].innerHTML
+      const secondCol = gridTiles[i + width].innerHTML
+      const thirdCol = gridTiles[i + width * 2].innerHTML
+      const fourthCol = gridTiles[i + width * 3].innerHTML
+      const col = [Number(firstCol), Number(secondCol), Number(thirdCol), Number(fourthCol)]
+
+      const sortedCol = col.filter(number => number > 0)
+      const emptyTile = 4 - sortedCol.length
+      const filling = Array(emptyTile).fill(0)
+      const newCol = sortedCol.concat(filling)
+      gridTiles[i].innerHTML = newCol[0]
+      gridTiles[i + width].innerHTML = newCol[1]
+      gridTiles[i + width * 2].innerHTML = newCol[2]
+      gridTiles[i + width * 3].innerHTML = newCol[3]
+    }
+  }
+
+
+  // adding the sum to the closest tile to the edge (with the sliding flow)
+  function sumRowTiles() {
+    for (let i = 0; i < 15; i++) {
+      if (gridTiles[i].innerHTML === gridTiles[i + 1].innerHTML) {
+        const total = Number(gridTiles[i].innerHTML) + Number(gridTiles[i + 1].innerHTML)
+        gridTiles[i].innerHTML = total
+      }
+    }
+  }
+  //event listerner
+  function sliding(event) {
+    if (event.keyCode === 39) {
+      slidingRight()
+    } else if (event.keyCode === 37) {
+      slidingLeft()
+    } else if (event.keyCode === 38) {
+      slidngUp()
+    } else if (event.keyCode === 40) {
+      slidingDown()
+    }
+  }
+  document.addEventListener('keyup', sliding)
+
+  function slidingRight() {
+    building4ArraysToRight()
+    sumRowTiles()
+    building4ArraysToRight()
+    adding2ToTheOpeningBoard()
+  }
+  function slidingLeft() {
+    building4ArraysToLeft()
+    sumRowTiles()
+    building4ArraysToLeft()
+  }
+  function slidngUp() {
+    building4ArraysToLeft()
+    sumRowTiles()
+    building4ArraysToLeft()
+  }
+  function slidingDown() {
+    building4ArraysToLeft()
+    sumRowTiles()
+    building4ArraysToLeft()
+  }
+
+})
