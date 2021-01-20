e5d9b7fadb00d68081f2c25d14faec8c947f02cf
yusuf963 <mohammad963yusuf@gmail.com>
2021-01-20 17:10:21 +0000

    fixing adding 2 bug and styling

--- app.js
+++ app.js
@@ -3,29 +3,46 @@ const button = document.getElementById('new-game')
 const score = document.getElementById('score')
 const audio = document.getElementById('audio')
 const camAudio = document.getElementById('cam-audio')
+const h4 = document.getElementById('user-name')
+const startGame = document.getElementById('start-game')
+const throwingTwo = document.getElementById('throwing-two')
+console.log(throwingTwo)
+//changeIt.style.visibility = 'hidden';
 
-console.log(audio)
+//storing user data 
+//Name
+const userName = prompt('Hi, please write you name')
+localStorage.setItem('data', userName)
+const storedUserName = localStorage.getItem('data')
+if (localStorage.getItem('data') !== 'null') {
+  h4.textContent = storedUserName
+} else {
+  alert('you playing the game as guest')
+  h4.textContent = 'Guest'
+}
+//Local Storage ends
 let points = 0
 const width = 4
 const gridTiles = []
 let randomTile
 let tile
 let count = 0
+let storedScore = 0
 // addin 2 or 4 to random tile position
 function adding2ToTheOpeningBoard(arr) {
   randomTile = gridTiles[Math.floor(Math.random() * gridTiles.length)]
   if (randomTile.innerHTML === '0') {
-    console.log(randomTile)
     randomTile.innerHTML = Math.random() < 0.5 ? arr[0] : arr[1]
+    startGame.play()
   } else {
     adding2ToTheOpeningBoard([2, 4])
+    startGame.play()
   }
 }
 function adding2() {
   randomTile = gridTiles[Math.floor(Math.random() * gridTiles.length)]
   if (randomTile.innerHTML === '0') {
     randomTile.innerHTML = 2
-    console.log(randomTile)
   } else {
     adding2()
   }
@@ -39,6 +56,7 @@ for (let i = 0; i < width * width; i++) {
   grid.append(tile)
   gridTiles.push(tile)
 }
+
 //event listener to through ranbdom 2 or 4 in a random tiles
 button.addEventListener('click', () => {
   gridTiles.forEach((tile) => {
@@ -47,8 +65,6 @@ button.addEventListener('click', () => {
   adding2ToTheOpeningBoard([2, 4])
   adding2ToTheOpeningBoard([2, 4])
   changeTileColor()
-
-
   //sliding the tils
   function building4ArraysToRight() {
     for (let i = 0; i < gridTiles.length; i++) {
@@ -141,6 +157,7 @@ button.addEventListener('click', () => {
         gridTiles[i + 1].innerHTML = 0
         points += total
         score.innerHTML = points
+        storedScore = points
       }
     }
 
@@ -154,6 +171,7 @@ button.addEventListener('click', () => {
         gridTiles[i + width].innerHTML = 0
         points += total
         score.innerHTML = points
+        storedScore = points
       }
     }
   }
@@ -163,6 +181,7 @@ button.addEventListener('click', () => {
       if (gridTiles[i].innerHTML === '2048') {
         alert('YOU WON')
         window.location.reload()
+        startGame.play()
       }
     }
   }
@@ -178,6 +197,7 @@ button.addEventListener('click', () => {
     if (count === 0) {
       alert('YOU LOST')
       window.location.reload()
+      startGame.play()
     }
   }
   lose()
@@ -196,7 +216,7 @@ button.addEventListener('click', () => {
 
   function takingPic() {
     for (let i = 0; i < gridTiles.length; i++) {
-      if (Number(gridTiles[i].innerHTML) === 8) {
+      if (Number(gridTiles[i].innerHTML) === 32) {
         if (count === 0) {
           alert('wow you reached high number, press ok to take a photo and continue playing, don\'t forget to simle🥳')
 
@@ -243,52 +263,50 @@ button.addEventListener('click', () => {
     }
   }
   document.addEventListener('keyup', sliding)
-
   function slidingRight() {
     building4ArraysToRight()
     sumRowTiles()
-    // adding2ToTheOpeningBoard([2, 2])
     adding2()
+    throwingTwo.play()
     changeTileColor()
     audio.play()
     win()
     lose()
     takingPic()
+    localStorage.setItem('dataPoints', storedScore)
   }
   function slidingLeft() {
     building4ArraysToLeft()
     sumRowTiles()
-    // adding2ToTheOpeningBoard([2, 2])
     adding2()
     changeTileColor()
     audio.play()
     win()
     lose()
     takingPic()
+    localStorage.setItem('dataPoints', storedScore)
   }
   function slidngUp() {
     building4ArraysToUp()
     sumColTiles()
-    // adding2ToTheOpeningBoard([2, 2])
     adding2()
     changeTileColor()
     audio.play()
     win()
     lose()
     takingPic()
+    localStorage.setItem('dataPoints', storedScore)
   }
   function slidingDown() {
     building4ArraysToDown()
     sumColTiles()
-    // adding2ToTheOpeningBoard([2, 2])
     adding2()
     changeTileColor()
     audio.play()
     win()
     lose()
     takingPic()
+    localStorage.setItem('dataPoints', storedScore)
   }
-
-  //changing tile color
-
+  alert('your last Score was ' + localStorage.dataPoints + ', try to beat it this time')
 })
