855b46c8c3767864d7c044eb9f51b321e09873f6
yusuf963 <mohammad963yusuf@gmail.com>
2021-01-20 13:35:45 +0000

    fixing adding 2 bug and styling

--- app.js
+++ app.js
@@ -1,126 +1,294 @@
-// gitting html tags
+const grid = document.getElementById('grid-container')
 const button = document.getElementById('new-game')
 const score = document.getElementById('score')
-const grid = document.getElementById('grid-container')
+const audio = document.getElementById('audio')
+const camAudio = document.getElementById('cam-audio')
 
-// open Board start
-// building the  grid tiles
+console.log(audio)
+let points = 0
 const width = 4
-let tiles = []
-console.log(tiles)
+const gridTiles = []
+let randomTile
 let tile
-let appendingRandomVAlueInRandomTile
-// rendering the grid tiles
+let count = 0
+// addin 2 or 4 to random tile position
+function adding2ToTheOpeningBoard(arr) {
+  randomTile = gridTiles[Math.floor(Math.random() * gridTiles.length)]
+  if (randomTile.innerHTML === '0') {
+    console.log(randomTile)
+    randomTile.innerHTML = Math.random() < 0.5 ? arr[0] : arr[1]
+  } else {
+    adding2ToTheOpeningBoard([2, 4])
+  }
+}
+function adding2() {
+  randomTile = gridTiles[Math.floor(Math.random() * gridTiles.length)]
+  if (randomTile.innerHTML === '0') {
+    randomTile.innerHTML = 2
+    console.log(randomTile)
+  } else {
+    adding2()
+  }
+}
+
+//building the grid at the opening board game
 for (let i = 0; i < width * width; i++) {
   tile = document.createElement('div')
-  grid.appendChild(tile)
   tile.innerHTML = 0
-  tile.id = i
   tile.classList.add('tile-styled')
-  tiles.push(tile)
+  grid.append(tile)
+  gridTiles.push(tile)
 }
-//starting the game by clicking on the start game button
+//event listener to through ranbdom 2 or 4 in a random tiles
 button.addEventListener('click', () => {
-  //reseting the grid tile to zero every time user click the start button
-  tiles.forEach(element => {
-    element.innerHTML = 0
+  gridTiles.forEach((tile) => {
+    tile.innerHTML = 0
   })
-  //mapping over the grid tiles New array 
-  const randomTile = tiles[Math.floor(Math.random() * tiles.length)]
-  // rendering 2 or 4 value as number in 2 random tiles
-  function appendingRandomVAlueInRandomTile(randomTileParameter) {
+  adding2ToTheOpeningBoard([2, 4])
+  adding2ToTheOpeningBoard([2, 4])
+  changeTileColor()
 
-    let valueOf2r4
-    if (randomTileParameter.innerHTML === '0') {
-      const arrayValue = [2, 4]
-      valueOf2r4 = arrayValue[Math.floor(Math.random() * arrayValue.length)]
-      //replacing the zero randomTileParameter with 2 or 4
-      randomTileParameter.innerHTML = valueOf2r4
-      console.log(tiles)
-    } else {
-      randomTileParameter = tiles[Math.floor(Math.random() * tiles.length)]
+
+  //sliding the tils
+  function building4ArraysToRight() {
+    for (let i = 0; i < gridTiles.length; i++) {
+      if (i % 4 === 0) {
+        const firstCol = gridTiles[i].innerHTML
+        const secondCol = gridTiles[i + 1].innerHTML
+        const thirdCol = gridTiles[i + 2].innerHTML
+        const fourthCol = gridTiles[i + 3].innerHTML
+        const rows = [Number(firstCol), Number(secondCol), Number(thirdCol), Number(fourthCol)]
+        const sortedRow = rows.filter(number => number > 0)
+        const emptyTile = 4 - sortedRow.length
+        const filling = Array(emptyTile).fill(0)
+        const newRow = filling.concat(sortedRow)
+        gridTiles[i].innerHTML = newRow[0]
+        gridTiles[i + 1].innerHTML = newRow[1]
+        gridTiles[i + 2].innerHTML = newRow[2]
+        gridTiles[i + 3].innerHTML = newRow[3]
+      }
     }
   }
-  appendingRandomVAlueInRandomTile(randomTile)
-  //mapping over the grid tiles New array 
-  let randomTileSd = tiles[Math.floor(Math.random() * tiles.length)]
-  if (randomTile === randomTileSd) {
-    randomTileSd = tiles[Math.floor(Math.random() * tiles.length)]
+
+  //sliding to left
+  function building4ArraysToLeft() {
+    for (let i = 0; i < gridTiles.length; i++) {
+      if (i % 4 === 0) {
+        const firstCol = gridTiles[i].innerHTML
+        const secondCol = gridTiles[i + 1].innerHTML
+        const thirdCol = gridTiles[i + 2].innerHTML
+        const fourthCol = gridTiles[i + 3].innerHTML
+        //giting neew array of number with 4 rows
+        const rows = [Number(firstCol), Number(secondCol), Number(thirdCol), Number(fourthCol)]
+        const sortedRow = rows.filter(number => number > 0)
+        const emptyTile = 4 - sortedRow.length
+        const filling = Array(emptyTile).fill(0)
+        const newRow = sortedRow.concat(filling)
+        gridTiles[i].innerHTML = newRow[0]
+        gridTiles[i + 1].innerHTML = newRow[1]
+        gridTiles[i + 2].innerHTML = newRow[2]
+        gridTiles[i + 3].innerHTML = newRow[3]
+      }
+    }
   }
-  appendingRandomVAlueInRandomTile(randomTileSd)
-  tiles = []
-  console.log(tiles)
+  // sliding down
+  function building4ArraysToDown() {
+    for (let i = 0; i < 4; i++) {
+      const firstCol = gridTiles[i].innerHTML
+      const secondCol = gridTiles[i + width].innerHTML
+      const thirdCol = gridTiles[i + width * 2].innerHTML
+      const fourthCol = gridTiles[i + width * 3].innerHTML
+      //giting neew array of number with 4 column
+      const col = [Number(firstCol), Number(secondCol), Number(thirdCol), Number(fourthCol)]
+
+      const sortedCol = col.filter(number => number > 0)
+      const emptyTile = 4 - sortedCol.length
+      const filling = Array(emptyTile).fill(0)
+      const newCol = filling.concat(sortedCol)
+      gridTiles[i].innerHTML = newCol[0]
+      gridTiles[i + width].innerHTML = newCol[1]
+      gridTiles[i + width * 2].innerHTML = newCol[2]
+      gridTiles[i + width * 3].innerHTML = newCol[3]
+    }
+  }
+
+  //sliding up 
+  function building4ArraysToUp() {
+    for (let i = 0; i < 4; i++) {
+      const firstCol = gridTiles[i].innerHTML
+      const secondCol = gridTiles[i + width].innerHTML
+      const thirdCol = gridTiles[i + width * 2].innerHTML
+      const fourthCol = gridTiles[i + width * 3].innerHTML
+      const col = [Number(firstCol), Number(secondCol), Number(thirdCol), Number(fourthCol)]
+
+      const sortedCol = col.filter(number => number > 0)
+      const emptyTile = 4 - sortedCol.length
+      const filling = Array(emptyTile).fill(0)
+      const newCol = sortedCol.concat(filling)
+      gridTiles[i].innerHTML = newCol[0]
+      gridTiles[i + width].innerHTML = newCol[1]
+      gridTiles[i + width * 2].innerHTML = newCol[2]
+      gridTiles[i + width * 3].innerHTML = newCol[3]
+    }
+  }
+
+  // adding the sum to the closest tile to the edge (with the sliding flow)
+  function sumRowTiles() {
+    for (let i = 0; i < 15; i++) {
+      if (gridTiles[i].innerHTML === gridTiles[i + 1].innerHTML) {
+        const total = Number(gridTiles[i].innerHTML) + Number(gridTiles[i + 1].innerHTML)
+        gridTiles[i].innerHTML = total
+        gridTiles[i + 1].innerHTML = 0
+        points += total
+        score.innerHTML = points
+      }
+    }
+
+  }
+  // adding the sum to the closest tile to the edge (with the sliding flow)
+  function sumColTiles() {
+    for (let i = 0; i < 12; i++) {
+      if (gridTiles[i].innerHTML === gridTiles[i + width].innerHTML) {
+        const total = Number(gridTiles[i].innerHTML) + Number(gridTiles[i + width].innerHTML)
+        gridTiles[i].innerHTML = total
+        gridTiles[i + width].innerHTML = 0
+        points += total
+        score.innerHTML = points
+      }
+    }
+  }
+
+  function win() {
+    for (let i = 0; i < gridTiles.length; i++) {
+      if (gridTiles[i].innerHTML === '2048') {
+        alert('YOU WON')
+        window.location.reload()
+      }
+    }
+  }
+  win()
+
+  function lose() {
+    let count = 0
+    for (let j = 0; j < gridTiles.length; j++) {
+      if (gridTiles[j].innerHTML === '0') {
+        count++
+      }
+    }
+    if (count === 0) {
+      alert('YOU LOST')
+      window.location.reload()
+    }
+  }
+  lose()
+  //event listerner
+  function sliding(event) {
+    if (event.keyCode === 39) {
+      slidingRight()
+    } else if (event.keyCode === 37) {
+      slidingLeft()
+    } else if (event.keyCode === 38) {
+      slidngUp()
+    } else if (event.keyCode === 40) {
+      slidingDown()
+    }
+  }
+
+  function takingPic() {
+    for (let i = 0; i < gridTiles.length; i++) {
+      if (Number(gridTiles[i].innerHTML) === 8) {
+        if (count === 0) {
+          alert('wow you reached high number, press ok to take a photo and continue playing, don\'t forget to simle🥳')
+
+          camAudio.play()
+          score.classList.add('flashing')
+        }
+        count++
+      }
+    }
+  }
+
+  function changeTileColor() {
+    for (let i = 0; i < gridTiles.length; i++) {
+      if (Number(gridTiles[i].innerHTML) === 8) {
+        gridTiles[i].classList.add('white')
+      } else if (Number(gridTiles[i].innerHTML) === 12) {
+        gridTiles[i].classList.remove('white')
+        gridTiles[i].classList.add('brown')
+      } else if (Number(gridTiles[i].innerHTML) === 16) {
+        gridTiles[i].classList.remove('brown')
+        gridTiles[i].classList.add('yellow')
+      } else if (Number(gridTiles[i].innerHTML) === 20) {
+        gridTiles[i].classList.remove('yellow')
+        gridTiles[i].classList.add('green')
+      } else if (Number(gridTiles[i].innerHTML) === 24) {
+        gridTiles[i].classList.remove('green')
+        gridTiles[i].classList.add('iris')
+      } else if (Number(gridTiles[i].innerHTML) === 28) {
+        gridTiles[i].classList.remove('iris')
+        gridTiles[i].classList.add('bamboo')
+      } else if (Number(gridTiles[i].innerHTML) === 32) {
+        gridTiles[i].classList.remove('bamboo')
+        gridTiles[i].classList.add('ecstasy')
+      } else if (Number(gridTiles[i].innerHTML) <= 6) {
+        gridTiles[i].classList.remove('red')
+        gridTiles[i].classList.remove('yellow')
+        gridTiles[i].classList.remove('white')
+        gridTiles[i].classList.remove('brown')
+        gridTiles[i].classList.remove('green')
+        gridTiles[i].classList.remove('iris')
+        gridTiles[i].classList.remove('bamboo')
+        gridTiles[i].classList.remove('ecstasy')
+      }
+    }
+  }
+  document.addEventListener('keyup', sliding)
+
+  function slidingRight() {
+    building4ArraysToRight()
+    sumRowTiles()
+    // adding2ToTheOpeningBoard([2, 2])
+    adding2()
+    changeTileColor()
+    audio.play()
+    win()
+    lose()
+    takingPic()
+  }
+  function slidingLeft() {
+    building4ArraysToLeft()
+    sumRowTiles()
+    // adding2ToTheOpeningBoard([2, 2])
+    adding2()
+    changeTileColor()
+    audio.play()
+    win()
+    lose()
+    takingPic()
+  }
+  function slidngUp() {
+    building4ArraysToUp()
+    sumColTiles()
+    // adding2ToTheOpeningBoard([2, 2])
+    adding2()
+    changeTileColor()
+    audio.play()
+    win()
+    lose()
+    takingPic()
+  }
+  function slidingDown() {
+    building4ArraysToDown()
+    sumColTiles()
+    // adding2ToTheOpeningBoard([2, 2])
+    adding2()
+    changeTileColor()
+    audio.play()
+    win()
+    lose()
+    takingPic()
+  }
+
+  //changing tile color
+
 })
-//open board finish
-
-
-// sliding rights, left, up and down starts
-
-
-//addin 2 during the game running
-// add 2 to the tile in the grid 
-//make sure the tile dosent contain number geater than 0
-grid.addEventListener('click', () => {
-  const arr = []
-  tiles.map((element) => {
-    if (element.innerHTML === '0') {
-      arr.push(element)
-    }
-  })
-  const adding2InRandomTile = Math.floor(Math.random() * arr.length)
-  arr[adding2InRandomTile].innerHTML = 2
-  // arr.map((element) => {
-  // if (!element.every()) {
-  //   console.log('its over')
-  // }
-  // })
-})
-
-let tstArray = []
-const firstRow = []
-const secondRow = []
-const thirdRow = []
-const fourthRow = []
-let i
-function building4Arrays() {
-  for (i = 0; i < tiles.length; i++) {
-    if (i < 4) {
-      firstRow.push(tiles[i].innerHTML)
-    } else if (i > 3 && i <= 7) {
-      secondRow.push(tiles[i].innerHTML)
-    } else if (i >= 8 && i < 12) {
-      thirdRow.push(tiles[i].innerHTML)
-    } else {
-      fourthRow.push(tiles[i].innerHTML)
-    }
-  }
-  const firstRowNum = firstRow.map((str) => Number(str))
-  const secondRowNum = secondRow.map((str) => Number(str))
-  const thirdRowNum = thirdRow.map((str) => Number(str))
-  const fourthRowNum = fourthRow.map((str) => Number(str))
-
-  tstArray = [firstRowNum, secondRowNum, thirdRowNum, fourthRowNum]
-  console.log(tstArray)
-  return tstArray
-}
-building4Arrays()
-
-
-
-
-
-//slidding
-// function userInput(e) {
-//   if (e.keyCode === 39) {
-//     console.log('reight key presed')
-//   } else if (e.keyCode === 37) {
-//     console.log('left key pressed')
-//   } else if (e.keyCode === 38) {
-//     console.log('up key pressed')
-//   } else if (e.keyCode === 40) {
-//     console.log('down key pressed')
-//   }
-// }
-// userInput()
-
-// window.addEventListener('keypress', userInput)
